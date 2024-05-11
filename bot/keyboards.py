cac296af4944558338395f2f4ac89dac5d7e5470
djokonda <djokonda-art@mail.ru>
2024-05-11 22:41:45 +0500

    Update keyboards.py

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,7 +1,7 @@
 from aiogram.types import WebAppInfo
 from aiogram import types
 
-web_app = WebAppInfo(url='https://djokonda-art.github.io/tg_mag_bot.github.io/site/')
+web_app = WebAppInfo(url='https://djokonda-art.github.io/tg_mag_bot.github.io/')
 
 keyboard = types.ReplyKeyboardMarkup(
     keyboard=[
